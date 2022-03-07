f0a0ea6a06e0da8df82b5db3ab3f003dcfb7ee3e
Raihan Rafi <rrafi0570@gmail.com>
2022-03-07 13:58:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// import {Navigate, Route, Routes} from "react-router-dom";
+// import Header from "./components/Header";
+// import Hello from "./components/Hello";
+// import Post from "./components/Post";
+// import Posts from "./components/Posts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Navigate to="/hello"/>} />
+//         <Route path="/hello/*" element={<Hello/>} >
+//             <Route path="world" element={<p>This is world!</p>} />
+//         </Route>
+//         <Route path="/posts" element={<Posts/>} />
+//         <Route path="/posts/:postId" element={<Post/>} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import axios from "axios";
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    dishes: null,
+    errMess: null,
+  };
+  componentDidMount() {
+    console.log("componentDidMount: ", this.state);
+    // axios
+    //   .get("http://localhost:3001/dishes")
+    //   .then((response) => response.data)
+    //   .then(data => {
+    //     this.setState({
+    //       dishes: data
+    //     })
+    //   })
+    //   .catch(error => {
+    //     this.setState({
+    //       errMess: error.message
+    //     })
+    //   });
+    // axios.post("http://localhost:3001/dishes", {name: "Pizza", price: "799"})
+    // .then(response => console.log(response))
+
+    // axios.put("http://localhost:3001/dishes/1", {name: "Cheese Cake", price: "250"})
+    // .then(response => console.log(response))
+
+    axios
+      .delete("http://localhost:3001/dishes/0")
+      .then((response) => console.log(response))
+      .catch((error) => {
+        this.setState({
+          errMess: error.message,
+        });
+      });
+  }
+
+  componentDidUpdate() {
+    console.log("Update: ", this.state);
+  }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default App;
